44cc71fe5feb5ed30fb79ac50acb6933e4c5eaf3
Glenn Dossot <glenn@dossot.net>
2024-11-19 14:57:24 -0800

    Added tag visibility to main.html

--- 1800_BBY11/scripts/main.js
+++ 1800_BBY11/scripts/main.js
@@ -3,8 +3,8 @@ function getNameFromAuth() {
         // Check if a user is signed in:
         if (user) {
             // Do something for the currently logged-in user here: 
-            console.log(user.uid); //print the uid in the browser console
-            console.log(user.displayName);  //print the user name in the browser console
+            //console.log(user.uid); //print the uid in the browser console
+            //console.log(user.displayName);  //print the user name in the browser console
             userName = user.displayName;
 
             //method #1:  insert with JS
@@ -14,7 +14,7 @@ function getNameFromAuth() {
             //$("#name-goes-here").text(userName); //using jquery
 
             //method #3:  insert using querySelector
-            document.querySelector("#name-goes-here").innerText = userName
+            //document.querySelector("#name-goes-here").innerText = userName
 
         } else {
             // No user is signed in.
@@ -30,8 +30,8 @@ getNameFromAuth(); //run the function
 function readQuote(day) {
     db.collection("quotes").doc(day)                                                         //name of the collection and documents should matach excatly with what you have in Firestore
         .onSnapshot(dayDoc => {                                                              //arrow notation
-            console.log("current document data: " + dayDoc.data());                          //.data() returns data object
-            document.getElementById("quote-goes-here").innerHTML = dayDoc.data().quote;      //using javascript to display the data on the right place
+            //console.log("current document data: " + dayDoc.data());                          //.data() returns data object
+            //document.getElementById("quote-goes-here").innerHTML = dayDoc.data().quote;      //using javascript to display the data on the right place
 
             //Here are other ways to access key-value data fields
             //$('#quote-goes-here').text(dayDoc.data().quote);         //using jquery object dot notation
@@ -44,40 +44,6 @@ function readQuote(day) {
 }
 readQuote("tuesday");        //calling the function
 
-function writeQuest() {
-    //define a variable for the collection you want to create in Firestore to populate data
-    var questsRef = db.collection("quests");
-
-    questsRef.add({
-        code: "01",
-        name: "Lawn Mowing", //replace with your own city?
-        city: "Burnaby",
-        province: "BC",
-        level: "easy",
-        details: "Mow my lawn for 50$",
-        last_updated: firebase.firestore.FieldValue.serverTimestamp()  //current system time
-    });
-    questsRef.add({
-        code: "02",
-        name: "Dog Walking", //replace with your own city?
-        city: "Anmore",
-        province: "BC",
-        level: "moderate",
-        details: "Golden Doodle need walking around 3pm for 40$",
-        last_updated: firebase.firestore.Timestamp.fromDate(new Date("March 10, 2022"))
-    });
-    questsRef.add({
-        code: "03",
-        name: "Gardening", //replace with your own city?
-        city: "North Vancouver",
-        province: "BC",
-        level: "hard",
-        details: "I need some labor help in my garden for 80$",
-        last_updated: firebase.firestore.Timestamp.fromDate(new Date("January 1, 2023"))
-    });
-}
-
-
 //------------------------------------------------------------------------------
 // Input parameter is a string representing the collection we are reading from
 //------------------------------------------------------------------------------
@@ -88,27 +54,37 @@ function displayCardsDynamically(collection) {
         .then(allQuest => {
             //var i = 1;  //Optional: if you want to have a unique ID for each hike
             allQuest.forEach(doc => { //iterate thru each doc
-                var title = doc.data().name;       // get value of the "name" key
-                var details = doc.data().details;  // get value of the "details" key
-                var code = doc.data().code;
-                var docID = doc.id;
-                let newcard = cardTemplate.content.cloneNode(true); // Clone the HTML template to create a new card (newcard) that will be filled with Firestore data.
-                
-                
+                title = doc.data().name;       // get value of the "name" key
+                details = doc.data().details;  // get value of the "details" key
+                questTags = doc.data().tags;
+                code = doc.data().code;
+                docID = doc.id;
+                newcard = cardTemplate.content.cloneNode(true); // Clone the HTML template to create a new card (newcard) that will be filled with Firestore data.
+
+
 
                 //update title and text and image
                 newcard.querySelector('.card-title').innerHTML = title;
                 newcard.querySelector('.card-text').innerHTML = details;
-                newcard.querySelector('.card-image').src = `./images/${code}.jpg`; //Example: NV01.jpg
+                newcard.querySelector('.card-image').src = `./images/${code}.jpg`;
+
+                let tags = ""
+                questTags.forEach((val) => {
+                    tags += `<span style="white-space:pre;background:red;border-radius:10%;display:inline-block">  ${val}  </span><span style="white-space:pre;">  </span>`
+                })
+
                 
-                newcard.querySelector('a').href = "eachQuest.html?docID="+docID;
+
+                newcard.querySelector('.card-tags').innerHTML = tags;
+
+                newcard.querySelector('a').href = "eachQuest.html?docID=" + docID;
                 newcard.querySelector('i').id = 'save-' + docID;   //guaranteed to be unique
 
                 newcard.querySelector('i').onclick = () => saveBookmark(docID);
 
-              
-
                 
+
+
                 //Optional: give unique ids to all elements for future use
                 // newcard.querySelector('.card-title').setAttribute("id", "ctitle" + i);
                 // newcard.querySelector('.card-text').setAttribute("id", "ctext" + i);
