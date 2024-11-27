4f3d876d7e9f0680a27f7a755aa7ba4b27db8f68
Glenn Dossot <glenn@dossot.net>
2024-11-26 16:36:47 -0800

    Changed image from blob to dataurl

--- 1800_BBY11/scripts/profile.js
+++ 1800_BBY11/scripts/profile.js
@@ -1,46 +1,3 @@
-//call the function to run it 
-
-/*
-function editUserInfo() {
-    //Enable the form fields
-    document.getElementById('personalInfoFields').disabled = false;
-}
-
-function saveUserInfo() {
-    //enter code here
-
-    //a) get user entered values
-    userName = document.getElementById('nameInput').value;       //get the value of the field with id="nameInput"
-    userSchool = document.getElementById('schoolInput').value;     //get the value of the field with id="schoolInput"
-    userCity = document.getElementById('cityInput').value;
-
-    //b) update user's document in Firestore
-    currentUser.update({
-        name: userName,
-        school: userSchool,
-        city: userCity
-    })
-        .then(() => {
-            console.log("Document successfully updated!");
-        })
-
-    //c) disable edit 
-    document.getElementById('personalInfoFields').disabled = true;
-}
-*/
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 var currentUser;               //points to the document of the user who is logged in
 
@@ -56,8 +13,6 @@ function populateUserInfo() {
                 .then(userDoc => {
                     //get the data fields of the user
                     let userName = userDoc.data().name;
-                    let userSchool = userDoc.data().school;
-                    let userCity = userDoc.data().city;
                     let pfp = userDoc.data().pfp;
 
                     //if the data fields are not empty, then write them in to the form.
@@ -88,10 +43,13 @@ window.addEventListener('load', function () {
     document.getElementById('pfp-upload').addEventListener('change', function () {
         if (this.files && this.files[0]) {
             resizeImage(URL.createObjectURL(this.files[0]), 500).then((data) => {
-                document.getElementById("profile-picture").src = data;
-                currentUser.set({
-                    pfp: data
-                },{ merge: true});
+                toDataURL(data).then(base64img => {
+                    document.getElementById("profile-picture").src = base64img;
+                    currentUser.set({
+                    pfp: base64img
+                    },{ merge: true});
+                })
+                
             })  
         }
         
